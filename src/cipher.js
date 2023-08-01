d0af6d4aa23e4dccc07439493ad30c04e98a24a2
Crystine Iokman <132957762+CrystineIokman639@users.noreply.github.com>
2023-08-01 11:19:01 -0300

    função de encode e decode funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,42 @@
+// Função para cifrar (criptografar) o texto com a Cifra de César
+function encode(deslocamento, texto) {
+  const alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let resultado = '';
+  texto = texto.toUpperCase();
+
+  for (let i = 0; i < texto.length; i++) {
+    let char = texto[i];
+    // Verifica se o caractere atual está no alfabeto (letra maiúscula)
+    if (alfabeto.indexOf(char) !== -1) {
+      const posicao = alfabeto.indexOf(char);
+
+      // Calcula a nova posição do caractere após o deslocamento
+      let novaPosicao = (posicao + deslocamento) % 26;
+      if (novaPosicao < 0) {
+        novaPosicao=26 + deslocamento + posicao;
+      }
+      // Obtém o caractere cifrado com base na nova posição
+      char = alfabeto[novaPosicao];
+    }
+    // Adiciona o caractere (cifrado ou não) ao resultado
+    resultado += char;
+  }
+  // Retorna o texto cifrado (resultado)
+  return resultado;
+}
+
+// Função para realizar o processo de decifragem (descriptografia)
+function decode(deslocamento, texto) {
+
+  // Chama a função "encode" com um deslocamento negativo para decifrar o texto
+  return cipher.encode(26 - deslocamento, texto);
+}
+
+// Objeto "cipher" contendo as funções "encode" e "decode" como propriedades
 const cipher = {
-  // ...
+  encode,
+  decode
 };
 
+// Exporta o objeto "cipher" para ser utilizado em outros módulos (apenas se estiver utilizando módulos)
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-import cipher from './cipher.js';
+import cipher from './cipher.js'
 
-console.log(cipher);
+// Função para lidar com o clique do botão e executar a criptografia
+function handleCifrar() {
+  const texto = document.getElementById('texto').value;
+  const deslocamento = parseInt(document.getElementById('deslocamento').value);
+  const resultadoElement = document.getElementById('resultado');
+  const resultado = cipher.encode(deslocamento, texto);
+  resultadoElement.textContent = resultado;
+}
+
+// Função para manipular o clique do botão e executar a descriptografia
+function handleDescifrar() {
+  const texto = document.getElementById('texto').value;
+  const deslocamento = parseInt(document.getElementById('deslocamento').value);
+  const resultadoElement = document.getElementById('resultado');
+  const resultado = cipher.decode(deslocamento, texto);
+  resultadoElement.textContent = resultado;
+}
+
+// Event listeners dos botoes
+document.querySelector('.cifrar-button').addEventListener('click', handleCifrar);
+document.querySelector('.descifrar-button').addEventListener('click', handleDescifrar);
